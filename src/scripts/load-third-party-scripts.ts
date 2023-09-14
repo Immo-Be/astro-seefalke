bfcc4e12d340c06d482d8fba9831c05aa6c1a1ef
Immo-Be <92583118+Immo-Be@users.noreply.github.com>
2023-09-14 16:16:30 +0200

    fix(google-maps): fix map key import

--- src/scripts/load-third-party-scripts.ts
+++ src/scripts/load-third-party-scripts.ts
@@ -4,8 +4,7 @@ import {loadGoogleMaps} from './google-map';
 
 export const loadThirdPartScripts = () => {
   loadGoogleMaps({
-    // key: import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY,
-    key: 'AIzaSyCx6NiDPw3rDhOGGRMtbX8SCaKmEwqWTB0',
+    key: import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY,
     v: 'weekly'
     // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
     // Add other bootstrap parameters as needed, using camel case.
